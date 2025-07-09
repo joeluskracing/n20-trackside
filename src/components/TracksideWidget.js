e0ea020e3cdf68ca62b77900261355ca85cb0982
joeluskracing <joeluskracing@gmail.com>
2025-07-08 22:35:00 -0400

    Check modal callback type before execution

--- src/components/TracksideWidget.js
+++ src/components/TracksideWidget.js
@@ -162,7 +162,7 @@ const TracksideWidget = () => {
 const handleModalOption = async (option) => {
     console.debug('Modal option selected:', option);
     const cb = modalCallback;
-    const hasCallback = Boolean(cb);
+    const hasCallback = typeof cb === 'function';
     console.debug('Has modal callback:', hasCallback);
     closeModal();
     if (hasCallback) {
